07c2c2ce6d7f3a3f8752f75b6aee29240f88b0b8
Carlos Mañas <carlos@spacenomads.com>
2019-08-06 21:05:39 +0200

    Add clue-comments

--- js/main.js
+++ js/main.js
@@ -36,8 +36,11 @@ fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-sema
 } 
 });
 
-
+// Pista 1: Aquí no puedes usar la variable local de otra función, 
+// lo que tienes que hacer es recoger el evento para saber 
+// cuál de todos a los que aplicaste el listener es el que dispara el click
 function showMe(){
+  // Pista 2: Aquí ten en cuenta que .toogle() ya te hace la comprobación del if ;)
   if (newsListeners.classList.contains ('news__item--no-image-visible')){
         newsListeners.classList.toggle ('news__item--no-image-visible');    
   }
